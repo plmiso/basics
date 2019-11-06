b351bbdc4451e5683df6d8896e5dc8fce15e8f64
plmiso <miloszsokolowski@aol.com>
2019-11-06 11:56:51 +0100

    two way binding

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = props => {
-  const [personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons:[
       {name:'Milosz', age:29},
-      {name:'Marek', age: 28}
-    ],
-  })
-  //as hooks dont merge but OVVERRIDE state, we need to diverse data to two states
-  const [otherState, setOtherState] = useState("some other state")
-
-  const switchNameHandler = () => {
-    setPersonsState({persons:[
-      {name:'Jedi', age:29},
-      {name:'Marek', age: 29}
+      {name:'Marek', age: 28},
+      {name: 'Monika', age: 25}
     ]
-  })
   }
 
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons:[
+      {name:newName, age:29},
+      {name:'Marek', age: 29},
+      {name: 'Monika', age: 25}
+    ]
+  }
+  )}
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+        {name: 'Milosz', age:29},
+        {name:event.target.value, age: 28},
+        {name: 'Monika', age: 25}
+      ]
+    })
+  }
+
+  render() {
     return (
       <div className="App">
          <h1>Siema</h1>
-         <button onClick={switchNameHandler}>Zmien imie</button>
-         <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> i to ta sama osoba</Person>
+         <button onClick={() => this.switchNameHandler("Jedi")}>Zmien imie</button>
+         <Person 
+         name={this.state.persons[0].name} 
+         age={this.state.persons[0].age}
+         click={this.switchNameHandler.bind(this, "Sith")}/>
+         <Person 
+         name={this.state.persons[1].name} 
+         age={this.state.persons[1].age}
+         changed={this.nameChangedHandler}> i to ta sama osoba</Person>
+          <Person 
+         name={this.state.persons[2].name} 
+         age={this.state.persons[2].age}> i to ta sama osoba</Person>
       </div>
     );
   }
+}
 
-
-export default app;
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,8 +6,9 @@ const person = (props) => {
     return(
     <div>
         {/* this.props.name kiedy używamy klasy jako podstawy komponentu */}
-        <p>Oto jestę {props.name} i mam {props.age} lat</p>
+        <p onClick={props.click}>Oto jestę {props.name} i mam {props.age} lat</p>
         <a>{props.children}</a>
+        <input type='text' onChange={props.changed} value={props.name}/>
     </div>
     )
 }
