1d38917158861ea0a06a4a77c88306393cf5133b
plmiso <miloszsokolowski@aol.com>
2019-11-06 11:17:36 +0100

    state replace by setState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons:[
+      {name:'Milosz', age:29},
+      {name:'Marek', age: 28}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({persons:[
+      {name:'Jedi', age:29},
+      {name:'Marek', age: 29}
+    ]
+  })
+  }
+
   render() {
-    // return (
-      // <div className="App">
-      //    <h1>Siema</h1>
-      // </div>
-      return React.createElement('div', {className:'App'}, React.createElement('h1',null, 'same shit different form'))
-    
+    return (
+      <div className="App">
+         <h1>Siema</h1>
+         <button onClick={this.switchNameHandler}>Zmien imie</button>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> i to ta sama osoba</Person>
+      </div>
+    );
   }
 }
 
