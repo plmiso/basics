c64545952c72a5edf38f054e87f11c44b2cc73a9
plmiso <miloszsokolowski@aol.com>
2019-11-06 11:34:22 +0100

    hooks intro

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState ] = useState({
     persons:[
       {name:'Milosz', age:29},
       {name:'Marek', age: 28}
-    ]
-  }
+    ],
+  })
+  //as hooks dont merge but OVVERRIDE state, we need to diverse data to two states
+  const [otherState, setOtherState] = useState("some other state")
 
-  switchNameHandler = () => {
-    this.setState({persons:[
+  const switchNameHandler = () => {
+    setPersonsState({persons:[
       {name:'Jedi', age:29},
       {name:'Marek', age: 29}
     ]
   })
   }
 
-  render() {
     return (
       <div className="App">
          <h1>Siema</h1>
-         <button onClick={this.switchNameHandler}>Zmien imie</button>
-         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> i to ta sama osoba</Person>
+         <button onClick={switchNameHandler}>Zmien imie</button>
+         <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> i to ta sama osoba</Person>
       </div>
     );
   }
-}
 
-export default App;
+
+export default app;
