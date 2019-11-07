6f496d2c0d7020f859f99c92176bfc146df03ac6
plmiso <miloszsokolowski@aol.com>
2019-11-07 15:29:59 +0100

    error boundry implemented for children component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ import classes from './Person.css'
 
 const person = (props) => {
     return (
+        
             <div className={classes.Person}>
                 {/* this.props.name kiedy używamy klasy jako podstawy komponentu */}
                 <p onClick={props.click}>Oto jestę {props.name} i mam {props.age} lat</p>
