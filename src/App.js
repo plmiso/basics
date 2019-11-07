b67528b6a72fe9041d2717ad58ca1d948b53e140
plmiso <miloszsokolowski@aol.com>
2019-11-07 15:42:02 +0100

    change of structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Person from './Persons/Person/Person'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
 
 class App extends Component {
@@ -50,13 +50,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return 
+            return (
               <Person
             click={() => this.deletePersonHandler(index)}
             changed = {(event) => this.nameChangedHandler(event,person.id)} 
             name={person.name} 
             age={person.age}
+            key={person.id}
             />
+            )
           })}
         </div>
       )
