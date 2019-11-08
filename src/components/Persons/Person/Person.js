d3624c481c6d1f6a595f1972ddf24fcf653e9519
plmiso <miloszsokolowski@aol.com>
2019-11-08 12:03:09 +0100

    auxiliray component as wrapper for reutrning multiple components in one component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Auxiliary from '../../../hoc/Auxiliary'
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-        [
+        <Auxiliary>
         /* this.props.name kiedy używamy klasy jako podstawy komponentu */
-        <p key ='i1' onClick={this.props.click}>
+        <p onClick={this.props.click}>
           Oto jestę {this.props.name} i mam {this.props.age} lat
         </p>,
-        <p key ='i2'>{this.props.children}</p>,
+        <p>{this.props.children}</p>,
         <input
-            key ='i3'
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-        ]
+        </Auxiliary>
     );
   }
 }
