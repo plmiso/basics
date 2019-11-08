e5bb1b06a8878bf24066cf2310562b4ee5cb2a59
plmiso <miloszsokolowski@aol.com>
2019-11-08 14:24:45 +0100

    reference in functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,15 +1,18 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+    const toggleBtnRef = useRef(null)
+
     useEffect(() =>{
         //odpala się za każdym razem kiedy komponent jest używany
         //spoko do http request
         console.log('[Cockpit.js] useEffect')
         // fake http
-        setTimeout(() => {
-            alert('Saved data to cloud!')
-        }, 1000)
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!')
+        // }, 1000)
+        toggleBtnRef.current.click()
         return () =>{
             
             //zwracanie pustej funkcji w useEffect pozwala na cleanup 
@@ -42,7 +45,7 @@ const cockpit = (props) => {
         <div className = {classes.Cockpit}>
             <h1>Siema</h1>
             <p className={assignedClasses.join(' ')}>Działa</p>
-            <button 
+            <button ref={toggleBtnRef}
             className={btnClass}
             onClick={props.clicked}>Toggle people</button>
         </div>
