4e6b20d026545734ffbfa1754f18abdc9e2222b1
plmiso <miloszsokolowski@aol.com>
2019-11-08 11:57:48 +0100

    use of Array as possibilty to return more than one JSX element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,18 +5,19 @@ class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
-        {/* this.props.name kiedy używamy klasy jako podstawy komponentu */}
-        <p onClick={this.props.click}>
+        [
+        /* this.props.name kiedy używamy klasy jako podstawy komponentu */
+        <p key ='i1' onClick={this.props.click}>
           Oto jestę {this.props.name} i mam {this.props.age} lat
-        </p>
-        <p>{this.props.children}</p>
+        </p>,
+        <p key ='i2'>{this.props.children}</p>,
         <input
+            key ='i3'
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+        ]
     );
   }
 }
