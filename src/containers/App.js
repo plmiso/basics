151b5933c1a8862e40e8c99046012784e0e0612a
plmiso <miloszsokolowski@aol.com>
2019-11-08 11:36:57 +0100

    React memo as good way to keep functional components update only when needed

--- src/containers/App.js
+++ src/containers/App.js
@@ -78,7 +78,7 @@ class App extends Component {
         { this.state.showCockpit ? (
           <Cockpit 
           showPersons = {this.state.showPersons}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           clicked = {this.togglePersonHandler}
           />
           ) : null}
